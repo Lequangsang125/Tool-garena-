0498f4cbf97cfa2b8ae4fd2cf460054d650bd27d
Le Quang Sang <sanglq125@gmail.com>
2025-03-15 21:23:19 +0700

    .

--- server-nodejs/index.js
+++ server-nodejs/index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const CryptoJS = require('crypto-js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // ✅ Cấu hình CORS
 app.use(cors({
