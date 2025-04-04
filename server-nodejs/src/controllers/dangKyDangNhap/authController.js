bfa92131f7152d3507f2072208ed02dabeb633d0
Le Quang Sang <sanglq125@gmail.com>
2025-04-04 15:51:12 +0700

    cho phép đọc cookie

--- server-nodejs/src/controllers/dangKyDangNhap/authController.js
+++ server-nodejs/src/controllers/dangKyDangNhap/authController.js
@@ -54,7 +54,7 @@ export const login = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: false,
-      secure: false,
+      secure: true,
       sameSite: "None",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
